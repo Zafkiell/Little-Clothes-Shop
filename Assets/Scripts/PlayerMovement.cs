89922caa9972906a4994af988055e5635a32272e
Luis Felipe Almeida Barros <felipe-almeida@hotmail.com.br>
2024-05-04 18:42:08 -0300

    Clothes Layers
    Clothes can be swapped in the Unity Editor, and animations work with base player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public Animator anim;
+    public Animator bodyAnim;
+    public Animator clothesAnim;
+    public Animator headPieceAnim;
 
     Vector2 movement;
     Vector2 lastDirection;
@@ -15,26 +17,45 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
-        if (movement.sqrMagnitude < .01)
-        {
-            lastDirection.x = anim.GetFloat("Horizontal");
-            lastDirection.y = anim.GetFloat("Vertical");
-
-            anim.SetFloat("Horizontal", lastDirection.x);
-            anim.SetFloat("Vertical", lastDirection.y);
-        }
-        else
-        {
-            anim.SetFloat("Horizontal", movement.x);
-            anim.SetFloat("Vertical", movement.y);
-        }
-
-        anim.SetFloat("Speed",movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
     {
+        SetAnimations();
+
         rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.deltaTime);
+    }
+
+    void SetAnimations()
+    {
+        if (movement.sqrMagnitude < .01)
+        {
+            lastDirection.x = bodyAnim.GetFloat("Horizontal");
+            lastDirection.y = bodyAnim.GetFloat("Vertical");
+
+            bodyAnim.SetFloat("Horizontal", lastDirection.x);
+            bodyAnim.SetFloat("Vertical", lastDirection.y);
+
+            clothesAnim.SetFloat("Horizontal", lastDirection.x);
+            clothesAnim.SetFloat("Vertical", lastDirection.y);
+
+            headPieceAnim.SetFloat("Horizontal", lastDirection.x);
+            headPieceAnim.SetFloat("Vertical", lastDirection.y);
+        }
+        else
+        {
+            bodyAnim.SetFloat("Horizontal", movement.x);
+            bodyAnim.SetFloat("Vertical", movement.y);
+
+            clothesAnim.SetFloat("Horizontal", movement.x);
+            clothesAnim.SetFloat("Vertical", movement.y);
+
+            headPieceAnim.SetFloat("Horizontal", movement.x);
+            headPieceAnim.SetFloat("Vertical", movement.y);
+        }
+
+        bodyAnim.SetFloat("Speed", movement.sqrMagnitude);
+        clothesAnim.SetFloat("Speed", movement.sqrMagnitude);
+        headPieceAnim.SetFloat("Speed", movement.sqrMagnitude);
     }
 }
